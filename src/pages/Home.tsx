3923893e2d7926bae026bf5ba573449127082a15
paularynty <paula.rynty@gmail.com>
2025-01-27 15:39:14 +0200

    added so_long image, transitioning to next.js next

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Project, Skill } from "../components";
+import so_long from "../styles/images/so_long.png";
 
 export const Home = () => {
   return (
@@ -9,12 +10,13 @@ export const Home = () => {
         <div className="body-text">
           <p>
             From human resources specialist, through copywriting and design, to
-            a frontend developer.
+            a budding frontend developer.
           </p>
           <br></br>
           <p>
-            Currently juggling programming studies at Hive Helsinki, may be
-            spotted crafting or café hopping (or both!) in her spare time.
+            Currently juggling programming studies at Hive Helsinki. May be
+            spotted crafting, climbing, or playing retro games in her spare
+            time.
           </p>
         </div>
       </div>
@@ -23,10 +25,14 @@ export const Home = () => {
         <div className="Projects-container">
           <Project
             name="Mikke!"
-            description="Kanji app made with React and TypeScript"
+            description="Kanji learning and lookup web app"
           />
-          <Project name="To-do list" description="To-do list app" />
-          <Project name="So_long" description="2D game made with C" />
+          <Project
+            name="Baby Dragon's Lunchtime"
+            description="2D game made with C"
+            image_path={so_long}
+          />
+          {/* <Project name="To-do list" description="To-do list app" /> */}
         </div>
       </div>
       <div className="Section-wrapper">
